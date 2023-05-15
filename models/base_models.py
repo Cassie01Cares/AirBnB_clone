d9def41501364ab3b7fb742b13a09f75f57803d0
Chukwunonso10 <kuzuechinonsojude@gmail.com>
2023-05-14 23:05:09 -0700

    models

--- models/base_models.py
+++
@@ -1,8 +0,0 @@
-#!/usr/bin/python3
-
-import uuid
-from datetime import datetime
-from models import storage
-
-class BaseModel:
-    """This will define all all the common attributes for other classes"""
